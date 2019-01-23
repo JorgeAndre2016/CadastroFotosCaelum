9e57c164f0da48431392d3673254f0716e3e5b1a
Jorge <jorge_aquino@hotmail.com.br>
2019-01-23 00:35:12 -0200

    Restruturação app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,4 @@ import { map } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Caelum Pic';
-  fotos = new Array<any>();
-
-  constructor(public http: HttpClient) {
-    this.http.get('http://localhost:3000/v1/fotos')
-     .pipe(map(resposta => resposta))
-      .subscribe((respo: Array<any>) => {
-        respo.forEach((resp) => {
-          this.fotos.push(resp);
-        });
-      }, (error) => {
-        console.log(error);
-      });
-  }
-}
+export class AppComponent { }
