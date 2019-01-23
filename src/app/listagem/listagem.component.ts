6ed07401f0d014271ac0ecb590d335d8f4aba44e
Jorge <jorge_aquino@hotmail.com.br>
2019-01-23 00:46:06 -0200

    Criação componente de listagem

---
+++ src/app/listagem/listagem.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Component({
+    selector: 'app-listagem',
+    templateUrl: './listagem.component.html'
+})
+
+export class ListagemComponent implements OnInit {
+
+    title = 'Caelum';
+    fotos = new Array<any>();
+
+    constructor(public http: HttpClient) {
+        this.http.get('http://localhost:3000/v1/fotos')
+            .pipe(map(resposta => resposta))
+            .subscribe((respo: Array<any>) => {
+                respo.forEach((resp) => {
+                    this.fotos.push(resp);
+                });
+            }, (error) => {
+                console.log(error);
+            });
+    }
+
+    public ngOnInit() { }
+
+}
