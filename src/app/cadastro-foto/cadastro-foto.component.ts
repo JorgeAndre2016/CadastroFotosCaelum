fe71b6564c696da534bd49f835f4437fef8c2314
Jorge <jorge_aquino@hotmail.com.br>
2019-01-23 00:45:44 -0200

    Criação componente de cadastro de foto

---
+++ src/app/cadastro-foto/cadastro-foto.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-cadastro-foto',
+    templateUrl: './cadastro-foto.component.html'
+})
+
+export class CadastroFotoComponent implements OnInit {
+
+    constructor() {}
+
+    public ngOnInit() { }
+}
